84ca7f3aa6c229d0c136248a2804b1865c61a9aa
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-09-08 19:20:32 -0500

    update

--- ultimate_react/steps/src/App.jsx
+++ ultimate_react/steps/src/App.jsx
@@ -1,7 +1,47 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react"
+
+const messages = [
+	"Learn React 🐛",
+	"Apply for jobs 😜",
+	"Invest your new income 🤮",
+]
+
 export default function App() {
+	const [step, setStep] = useState(1)
+
+	function handlePrevious() {
+		if (step > 1) setStep(step - 1)
+		{
+			test.name = "fred"
+		}
+	}
+
+	function handleNext() {
+		if (step < 3) setStep(step + 1)
+	}
+
 	return (
-		<>
-			<div>Hello</div>
-		</>
+		<div className="steps">
+			<div className="numbers">
+				<div className={`${step >= 1 ? "active" : ""}`}>1</div>
+				<div className={`${step >= 2 ? "active" : ""}`}>2</div>
+				<div className={`${step >= 3 ? "active" : ""}`}>3</div>
+			</div>
+
+			<p className="message">
+				Step {step}: {messages[step - 1]}
+			</p>
+
+			<div className="buttons">
+				<button
+					style={{ backgroundColor: "#7950f2", color: "#fff" }}
+					onClick={handlePrevious}
+				>
+					Previous
+				</button>
+				<button onClick={handleNext}>Next</button>
+			</div>
+		</div>
 	)
 }
