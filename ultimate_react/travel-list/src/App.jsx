3566bd73a8d889f9c7e15591b84d3d047497f009
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-09-09 18:37:44 -0500

    update

--- ultimate_react/travel-list/src/App.jsx
+++ ultimate_react/travel-list/src/App.jsx
@@ -1,8 +1,135 @@
+/* eslint-disable react/prop-types */
+
+import { useState } from "react"
+
 function App() {
+	const [items, setItems] = useState([])
+
+	function handleAddItems(item) {
+		setItems((items) => [...items, item])
+	}
+
+	// Delete items
+	function handleDeleteItem(id) {
+		setItems((items) => items.filter((item) => item.id !== id))
+	}
+
+	function handleToggleItem(id) {
+		setItems((items) =>
+			items.map((item) =>
+				item.id === id ? { ...item, packed: !item.packed } : item
+			)
+		)
+	}
+
 	return (
-		<>
-			<div className="header">Hello</div>
-		</>
+		<div className="app">
+			<Logo />
+			<Form onAddItems={handleAddItems} />
+			<PackingList
+				items={items}
+				onDeleteItem={handleDeleteItem}
+				onToggleItems={handleToggleItem}
+			/>
+			<Stats items={items} />
+		</div>
+	)
+}
+
+function Logo() {
+	return <h1>Far Away 🤮</h1>
+}
+
+function Form({ onAddItems }) {
+	const [description, setDescription] = useState("test")
+	const [quantity, setQuantity] = useState(1)
+
+	function handleSubmit(e) {
+		e.preventDefault()
+
+		if (!description) return
+
+		const newItem = { description, quantity, packed: false, id: Date.now() }
+		console.log(newItem)
+
+		onAddItems(newItem)
+
+		setDescription("")
+		setQuantity(1)
+	}
+
+	return (
+		<form className="add-form" onSubmit={handleSubmit}>
+			<h3>What do you need for your 🐛 trip?</h3>
+
+			<select
+				value={quantity}
+				onChange={(e) => setQuantity(Number(e.target.value))}
+			>
+				{Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+					<option value={num} key={num}>
+						{num}
+					</option>
+				))}
+			</select>
+			<input
+				type="text"
+				placeholder="Item..."
+				value={description}
+				onChange={(e) => setDescription(e.target.value)}
+			/>
+			<button>Add</button>
+		</form>
+	)
+}
+
+function PackingList({ items, onDeleteItem, onToggleItems }) {
+	return (
+		<div>
+			<div className="list">
+				{items.map((item) => (
+					<Item
+						item={item}
+						onDeleteItem={onDeleteItem}
+						onToggleItems={onToggleItems}
+						key={item.key}
+					></Item>
+				))}
+			</div>
+		</div>
+	)
+}
+
+function Item({ item, onDeleteItem, onToggleItems }) {
+	return (
+		<li>
+			<input
+				type="checkbox"
+				value={item.packed}
+				onChange={() => onToggleItems(item.id)}
+			/>
+			<span style={item.packed ? { textDecoration: "line-through" } : {}}>
+				{item.quantity} {item.description}
+			</span>
+			<button onClick={() => onDeleteItem(item.id)}>❌</button>
+		</li>
+	)
+}
+
+function Stats({ items }) {
+	const numItems = items.length
+	const numPacked = items.filter((item) => item.packed).length
+	const percentage = Math.round((numPacked / numItems) * 100)
+
+	return (
+		<footer className="stats">
+			<em>
+				{percentage === 100
+					? "You got everything! Ready to go!"
+					: `You have ${numItems} items on your list, and you already packed
+				${numPacked} (${percentage}%)`}
+			</em>
+		</footer>
 	)
 }
 
