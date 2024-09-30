c1a6ad53d52db5b271ee940335d2e6f1af391205
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-09-30 17:52:43 -0500

    add StarRating component

--- ultimate_react/popcorn/src/App.jsx
+++ ultimate_react/popcorn/src/App.jsx
@@ -1,5 +1,6 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
+
 import { useState } from "react";
 
 const tempMovieData = [
@@ -53,6 +54,7 @@ const average = (arr) =>
 
 export default function App() {
 	const [movies, setMovies] = useState(tempMovieData);
+	const [watched, setWatched] = useState(tempWatchedData);
 
 	return (
 		<>
@@ -61,10 +63,13 @@ export default function App() {
 				<NumResults movies={movies} />
 			</NavBar>
 			<Main>
-				<ListBox>
+				<Box>
 					<MovieList movies={movies} />
-				</ListBox>
-				<WatchedBox />
+				</Box>
+				<Box>
+					<WatchedSummary watched={watched} />
+					<WatchedMovieList watched={watched} />
+				</Box>
 			</Main>
 		</>
 	);
@@ -115,18 +120,15 @@ function Main({ children }) {
 	return <main className="main">{children}</main>;
 }
 
-function ListBox({ children }) {
-	const [isOpen1, setIsOpen1] = useState(true);
+function Box({ children }) {
+	const [isOpen, setIsOpen] = useState(true);
 
 	return (
 		<div className="box">
-			<button
-				className="btn-toggle"
-				onClick={() => setIsOpen1((open) => !open)}
-			>
-				{isOpen1 ? "–" : "+"}
+			<button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+				{isOpen ? "–" : "+"}
 			</button>
-			{isOpen1 && children}
+			{isOpen && children}
 		</div>
 	);
 }
@@ -156,7 +158,7 @@ function Movie({ movie }) {
 	);
 }
 
-function WatchedBox() {
+/* function WatchedBox() {
 	const [watched, setWatched] = useState(tempWatchedData);
 	const [isOpen2, setIsOpen2] = useState(true);
 
@@ -176,7 +178,7 @@ function WatchedBox() {
 			)}
 		</div>
 	);
-}
+} */
 
 function WatchedSummary({ watched }) {
 	const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
---
+++ ultimate_react/popcorn/src/StarRating.jsx
@@ -0,0 +1,3 @@
+export default function StarRating() {
+	return <div>fffffffffff</div>;
+}
