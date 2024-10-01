0f7baa52587c55dca889254bc9b2b283162e3c6b
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-09-30 19:36:29 -0500

    Create StarRating component

--- ultimate_react/popcorn/src/main.jsx
+++ ultimate_react/popcorn/src/main.jsx
@@ -6,6 +6,8 @@ import StarRating from "./StarRating.jsx";
 
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
+		<StarRating maxRating={5} />
+		<StarRating maxRating={10} />
 		<StarRating />
 		{/* 		<App />
 		 */}{" "}
