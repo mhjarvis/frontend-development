320116d17dd34ac19bbd878b4a169dabbffacf6e
Markus Jarvis <markus.h.jarvis@gmail.com>
2024-11-10 15:46:17 -0600

    Reinstate original popcorn app

--- ultimate_react/popcorn/src/main.jsx
+++ ultimate_react/popcorn/src/main.jsx
@@ -1,12 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-/* import App from "./App.jsx";
-import "./index.css"; */
-import StarRating from "./StarRating.jsx";
-
+import App from "./App.jsx";
+import "./index.css";
+/* import StarRating from "./StarRating.jsx";
+ */
 createRoot(document.getElementById("root")).render(
 	<StrictMode>
-		<StarRating
+		<App />
+		{/* 		<StarRating
 			maxRating={5}
 			className="test"
 			messages={["Terrible", "Good", "Poop", "Sure", "Greatish"]}
@@ -16,5 +17,6 @@ createRoot(document.getElementById("root")).render(
 		<StarRating size={24} />
 		{/* 		<App />
 		 */}{" "}
+		*/
 	</StrictMode>
 );
