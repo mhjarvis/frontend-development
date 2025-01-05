4a0c3843ba4b3b2e0ac777666421b7d1f2d645c0
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-01-05 16:33:44 -0600

    Update document title

--- ultimate_react/popcorn/src/App.jsx
+++ ultimate_react/popcorn/src/App.jsx
@@ -78,6 +78,14 @@ export default function App() {
 		setSelectedId(null)
 	}
 
+	function handleAddWatched(movie) {
+		setWatched((watched) => [...watched, movie])
+	}
+
+	function handleDeleteWatched(id) {
+		setWatched((watched) => watched.filter((movie) => movie.imdbID !== id))
+	}
+
 	useEffect(() => {
 		async function fetchMovies() {
 			try {
@@ -137,12 +145,17 @@ export default function App() {
 						<MovieDetails
 							selectedId={selectedId}
 							onCloseMovie={handleCloseMovie}
+							onAddWatched={handleAddWatched}
+							watched={watched}
 						/>
 					) : (
 						<>
 							{" "}
 							<WatchedSummary watched={watched} />
-							<WatchedMovieList watched={watched} />
+							<WatchedMovieList
+								watched={watched}
+								onDeleteWatched={handleDeleteWatched}
+							/>
 						</>
 					)}
 				</Box>
@@ -266,9 +279,15 @@ function Movie({ movie, onSelectMovie }) {
 	);
 } */
 
-function MovieDetails({ selectedId, onCloseMovie }) {
+function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 	const [movie, setMovie] = useState({})
 	const [isLoading, setIsLoading] = useState(false)
+	const [userRating, setUserRating] = useState("")
+
+	const isWatched = watched.map((movie) => movie.imdbID).includes(selectedId)
+	const watchedUserRating = watched.find(
+		(movie) => movie.imdbID === selectedId
+	)?.userRating
 
 	const {
 		Title: title,
@@ -283,7 +302,19 @@ function MovieDetails({ selectedId, onCloseMovie }) {
 		Genre: genre,
 	} = movie
 
-	console.log(title, year)
+	function handleAdd() {
+		const newWatchedMovie = {
+			imdbID: selectedId,
+			title,
+			year,
+			poster,
+			imdbRating: Number(imdbRating),
+			runtime: Number(runtime.split(" ").at(0)),
+			userRating,
+		}
+		onAddWatched(newWatchedMovie)
+		onCloseMovie()
+	}
 
 	useEffect(
 		function () {
@@ -299,6 +330,14 @@ function MovieDetails({ selectedId, onCloseMovie }) {
 			getMovieDetails()
 		},
 		[selectedId]
+	)
+
+	useEffect(
+		function () {
+			if (!title) return
+			document.title = `Movie | ${title}`
+		},
+		[title]
 	)
 
 	return (
@@ -326,7 +365,26 @@ function MovieDetails({ selectedId, onCloseMovie }) {
 					</header>
 					<section>
 						<div className="rating">
-							<StarRating maxRating={10} size={24} />
+							{!isWatched ? (
+								<>
+									<StarRating
+										maxRating={10}
+										size={24}
+										onSetRating={setUserRating}
+									/>
+									{userRating > 0 ? (
+										<button className="btn-add" onClick={handleAdd}>
+											+ Add to list
+										</button>
+									) : (
+										""
+									)}
+								</>
+							) : (
+								<p>
+									You rated this movie {watchedUserRating} <span>⭐️</span>
+								</p>
+							)}
 						</div>
 						<p>
 							<em>{plot}</em>
@@ -355,11 +413,11 @@ function WatchedSummary({ watched }) {
 				</p>
 				<p>
 					<span>⭐️</span>
-					<span>{avgImdbRating}</span>
+					<span>{avgImdbRating.toFixed(2)}</span>
 				</p>
 				<p>
 					<span>🌟</span>
-					<span>{avgUserRating}</span>
+					<span>{avgUserRating.toFixed(2)}</span>
 				</p>
 				<p>
 					<span>⏳</span>
@@ -370,21 +428,25 @@ function WatchedSummary({ watched }) {
 	)
 }
 
-function WatchedMovieList({ watched }) {
+function WatchedMovieList({ watched, onDeleteWatched }) {
 	return (
 		<ul className="list">
 			{watched.map((movie) => (
-				<WatchedMovie movie={movie} key={movie.imdbID} />
+				<WatchedMovie
+					movie={movie}
+					key={movie.imdbID}
+					onDeleteWatched={onDeleteWatched}
+				/>
 			))}
 		</ul>
 	)
 }
 
-function WatchedMovie({ movie }) {
+function WatchedMovie({ movie, onDeleteWatched }) {
 	return (
 		<li key={movie.imdbID}>
-			<img src={movie.Poster} alt={`${movie.Title} poster`} />
-			<h3>{movie.Title}</h3>
+			<img src={movie.poster} alt={`${movie.title} poster`} />
+			<h3>{movie.title}</h3>
 			<div>
 				<p>
 					<span>⭐️</span>
@@ -398,6 +460,13 @@ function WatchedMovie({ movie }) {
 					<span>⏳</span>
 					<span>{movie.runtime} min</span>
 				</p>
+
+				<button
+					className="btn-delete"
+					onClick={() => onDeleteWatched(movie.imdbID)}
+				>
+					x
+				</button>
 			</div>
 		</li>
 	)
--- ultimate_react/popcorn/src/StarRating.jsx
+++ ultimate_react/popcorn/src/StarRating.jsx
@@ -1,17 +1,16 @@
 /* eslint-disable react/prop-types */
-
-import { useState } from "react";
-import PropTypes from "prop-types";
+import { useState } from "react"
+import PropTypes from "prop-types"
 
 const containerStyle = {
 	display: "flex",
 	alignItems: "center",
 	gap: "16px",
-};
+}
 
 const starContainerStyle = {
 	display: "flex",
-};
+}
 
 StarRating.propTypes = {
 	maxRating: PropTypes.number,
@@ -21,34 +20,40 @@ StarRating.propTypes = {
 	messages: PropTypes.array,
 	className: PropTypes.string,
 	onSetRating: PropTypes.func,
-};
+}
 
 export default function StarRating({
-	maxRating = 10,
+	maxRating = 5,
 	color = "#fcc419",
 	size = 48,
-	test = "",
+	className = "",
 	messages = [],
 	defaultRating = 0,
+	onSetRating,
 }) {
-	const [rating, setRating] = useState(defaultRating);
-	const [tempRating, setTempRating] = useState(0);
+	const [rating, setRating] = useState(defaultRating)
+	const [tempRating, setTempRating] = useState(0)
+
+	function handleRating(rating) {
+		setRating(rating)
+		onSetRating(rating)
+	}
 
 	const textStyle = {
 		lineHeight: "1",
 		margin: "0",
 		color,
 		fontSize: `${size / 1.5}px`,
-	};
+	}
 
 	return (
-		<div style={containerStyle} className={test}>
+		<div style={containerStyle} className={className}>
 			<div style={starContainerStyle}>
 				{Array.from({ length: maxRating }, (_, i) => (
 					<Star
 						key={i}
-						onRate={() => setRating(i + 1)}
 						full={tempRating ? tempRating >= i + 1 : rating >= i + 1}
+						onRate={() => handleRating(i + 1)}
 						onHoverIn={() => setTempRating(i + 1)}
 						onHoverOut={() => setTempRating(0)}
 						color={color}
@@ -62,7 +67,7 @@ export default function StarRating({
 					: tempRating || rating || ""}
 			</p>
 		</div>
-	);
+	)
 }
 
 function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
@@ -71,7 +76,7 @@ function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
 		height: `${size}px`,
 		display: "block",
 		cursor: "pointer",
-	};
+	}
 
 	return (
 		<span
@@ -106,5 +111,5 @@ function Star({ onRate, full, onHoverIn, onHoverOut, color, size }) {
 				</svg>
 			)}
 		</span>
-	);
+	)
 }
