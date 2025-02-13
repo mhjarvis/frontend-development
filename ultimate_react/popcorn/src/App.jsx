76220f15b33ce0684ee1185bb208ffaa7a3f9838
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-02-12 20:33:20 -0600

    Add cleanup function to reset document.title

--- ultimate_react/popcorn/src/App.jsx
+++ ultimate_react/popcorn/src/App.jsx
@@ -336,6 +336,11 @@ function MovieDetails({ selectedId, onCloseMovie, onAddWatched, watched }) {
 		function () {
 			if (!title) return
 			document.title = `Movie | ${title}`
+
+			// cleanup function
+			return function () {
+				document.title = "usePopcorn"
+			}
 		},
 		[title]
 	)
