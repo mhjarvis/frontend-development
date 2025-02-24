af383d4a326d9a763c27f863741c6d4ca2a7aef2
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-02-24 16:02:38 -0600

    Add worldview project

--- testing/src/App.jsx
+++ testing/src/App.jsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable react/prop-types */
+import { useState } from "react"
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+	const [left, setLeft] = useState(0)
+	const [right, setRight] = useState(0)
+	const [allClicks, setAll] = useState([])
+	const [total, setTotal] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const handleLeftClick = () => {
+		setAll(allClicks.concat("L"))
+		const updatedLeft = left + 1
+		setLeft(updatedLeft)
+		setTotal(updatedLeft + right)
+	}
+
+	const handleRightClick = () => {
+		setAll(allClicks.concat("R"))
+		const updatedRight = right + 1
+		setRight(updatedRight)
+		setTotal(left + updatedRight)
+	}
+
+	return (
+		<div>
+			{left}
+			<Button
+				onClick={() => {
+					setLeft(0)
+					setRight(0)
+					setAll([])
+					setTotal(0)
+				}}
+				text="reset"
+			/>
+			<Button onClick={handleLeftClick} text="left" />
+			<Button onClick={handleRightClick} text="right" />
+			{right}
+			<History allClicks={allClicks} />
+		</div>
+	)
 }
 
+const History = (props) => {
+	if (props.allClicks.length === 0) {
+		return <div>the app is used by pressing the buttons</div>
+	}
+	return <div>button press history: {props.allClicks.join(" ")}</div>
+}
 export default App
+
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
