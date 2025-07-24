f479a495037bd35d828fb7a649d6ac54d660f06c
Markus Jarvis <markus.h.jarvis@gmail.com>
2025-07-24 08:39:22 -0500

    Add pricing grids page

---
+++ nextjs-tailwind-projects/app/pricing-grids/page.jsx
@@ -0,0 +1,16 @@
+export default function PricingGridsPage() {
+	return (
+		<div>
+			<h1>Pricing Grids</h1>
+			<p>
+				This page will display various pricing grids for different products.
+			</p>
+			{/* Add your pricing grid components here */}
+		</div>
+	)
+}
+export const metadata = {
+	title: "Pricing Grids",
+	description: "Explore our pricing grids for various products and services.",
+	keywords: "pricing, grids, products, services",
+}
